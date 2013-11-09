980aa22718a25a55cbef0227479916907dce14e8
Justin.Dragos <Justin.Dragos@gmail.com>
2013-11-09 17:52:52 +0000

    Added in Javadoc for the annotation.

--- src/main/java/example/jsr/annotations/Anagram.java
+++ src/main/java/example/jsr/annotations/Anagram.java
@@ -13,15 +13,31 @@ import org.hibernate.validator.constraints.NotBlank;
 
 import example.jsr.validators.AnagramIntegerValidator;
 import example.jsr.validators.AnagramStringValidator;
-
+/**
+* Anagram is reponsible for validating that the annotated field is an anagram. In order to comply the given field must be the same forwards and backwards. Captialization and 
+* whitespace does matter.
+* <p> Supports:
+* <ul>
+*  <li>String</li>
+*  <li>Integer</li>
+* </ul>
+*  
+*
+**/
 @Target({ElementType.FIELD, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Constraint(validatedBy = {AnagramStringValidator.class, AnagramIntegerValidator.class})
 @NotBlank
 public @interface Anagram {
+    /**
+    * Message to be displayed if this field is in violation of the rule.
+    **/
     String message() default "Not a ton";
 
+    /**
+    * Array of marker classes used to demark which group of validations in which this constraint should be included.
+    **/
     Class<?>[] groups() default {};
 
     Class<? extends Payload>[] payload() default {};
